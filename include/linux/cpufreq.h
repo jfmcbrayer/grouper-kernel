461ae971dd0ce48549125c10312ea7a41a8ad43d
Ezekeel <notezekeel@googlemail.com>
2012-07-07 00:19:07 +0200

    Added CPUfreq governor 'wheatley'.

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -373,6 +373,9 @@ extern struct cpufreq_governor cpufreq_gov_interactive;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SMARTASS2)
 extern struct cpufreq_governor cpufreq_gov_smartass2;
 #define CPUFREQ_DEFAULT_GOVERNOR (&cpufreq_gov_smartass2)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_WHEATLEY)
+extern struct cpufreq_governor cpufreq_gov_wheatley;
+#define CPUFREQ_DEFAULT_GOVERNOR  (&cpufreq_gov_wheatley)
 #endif
 
 
